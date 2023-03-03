f54f6ab5704da87a5349c7951e10733a08f14a89
David Stekol <dstekol@gmail.com>
2023-03-02 20:44:01 -0500

    test

---
+++ IdeaProjects/actualMavenTest/src/main/java/org/example/DataAccessObject.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.sql.*;
+import java.util.List;
+
+public abstract class DataAccessObject  {
+
+    protected final Connection connection;
+    protected final static String LAST_VAL = "SELECT last_value FROM ";
+    protected final static String PLAYER_SEQUENCE = "player_seq";
+
+    public DataAccessObject(Connection connection){
+        super();
+        this.connection = connection;
+    }
+
+    // public abstract T findById(long id);
+}
