c42d75f3547f27b2de91b1922ea3c3d9729d8e7e
Supernova2002 <dstekol@gmail.com>
2023-03-04 03:17:59 -0500

    All setting DAOs completed, use instructions in the JDBCExecutor.Java file in src/main/org/example. Next step is integration and retrieval DAOs, which can be done concurrently.

---
+++ IdeaProjects/actualMavenTest/src/main/java/org/example/Key.java
@@ -0,0 +1,43 @@
+package org.example;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Key implements DataTransferObject {
+
+    public Key(String name, ArrayList<Integer> inList){
+        signalName = name;
+        bucketCutoffs = inList;
+
+    }
+
+
+
+    private String signalName;
+
+    private List<Integer> bucketCutoffs = new ArrayList<Integer>();
+
+    public void addBucketCutoff(int cutoff) {
+        this.bucketCutoffs.add(cutoff);
+    }
+    public List<Integer> getBucketCutoff(){
+        return bucketCutoffs;
+    }
+
+    public String getSignalName() {
+        return signalName;
+    }
+
+    public void setSignalName(String signalName) {
+        this.signalName = signalName;
+    }
+
+
+
+
+
+
+
+
+}
