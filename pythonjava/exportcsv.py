f9beaf48038bab6b73961a7828547b1158741f8d
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-03-05 23:10:35 +0000

    Get list of CSV file paths from mdf

--- pythonjava/exportcsv.py
+++ pythonjava/exportcsv.py
@@ -1,16 +1,22 @@
-from sys import stdout
+import os
+import sys
+import uuid
+
 from asammdf import MDF, Signal
 
 def main():
     input_mdf = sys.argv[1]
-    input_dbc = sys.argv[2]
-    print(input_mdf)
-    print(input_dbc)
+    _input_dbc = sys.argv[2] # currently unused
+
     mdf = MDF(input_mdf)
-    databases = {"CAN": [(input_dbc, 1)]}
-    extracted = mdf.extract_bus_logging(database_files=databases)
-    speed = extracted.get("EngineSpeed")
-    extracted.export('csv', "out.csv")
+
+    unique_id = uuid.uuid4()
+    dir = f'out_{unique_id}'
+    os.mkdir(dir)
+
+    mdf.export('csv', f'out_{unique_id}/out.csv')
+
+    print(dir)
 
 
 if __name__ == '__main__':
--- pythonjava/import_mdf.java
+++ pythonjava/import_mdf.java
@@ -1,36 +1,52 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.util.*;
+import java.io.File;
+import java.io.InputStreamReader;
 import java.lang.Process;
 import java.lang.ProcessBuilder;
 
 public class import_mdf {
     public static void main(String[] args) throws Exception {
-        Path mdf = Paths.get("./sdf");
-        Path dbc = Paths.get("./sdsdsd");
-        convertMdfToCsvFiles(mdf, dbc);
+        Path mdf = Paths.get("./examples/audi/output1.mf4");
+        Path dbc = Paths.get("./examples/audi/audi.dbc");
+        System.out.println(convertMdfToCsvFiles(mdf, dbc));
     }
 
-    public static String[] convertMdfToCsvFiles(Path mf4, Path dbc) {
-        try {
-            Process mdfpy = new ProcessBuilder(
-                "python3",
-                "exportcsv.py",
-                mf4.toAbsolutePath().toString(),
-                dbc.toAbsolutePath().toString()
-            ).start();
+    public static List<String> convertMdfToCsvFiles(Path mf4, Path dbc) throws Exception {
+        // call python to convert mdf to .csv files
+        Process mdfpy = new ProcessBuilder(
+            "python3",
+            "exportcsv.py",
+            mf4.toAbsolutePath().toString(),
+            dbc.toAbsolutePath().toString()
+        ).start();
 
-            // has the process exited successfully?
-            if (mdfpy.waitFor() != 0) {
-                // Dump stderr to a string and throw an exception.
-                String err = new String(mdfpy.getErrorStream().readAllBytes());
-                throw new Exception("exportcsv failed. stderr follows:-----\n" + err + "\n-----");
-            }
-        } catch (Exception e) {
-            System.err.println("Caught exception: " + e.toString());
-            System.exit(-1);
+        // has the process exited successfully?
+        if (mdfpy.waitFor() != 0) {
+            // Dump stderr to a string and throw an exception.
+            String err = new String(mdfpy.getErrorStream().readAllBytes());
+            throw new Exception("exportcsv failed. stderr follows:-----\n" + err + "\n-----");
         }
 
-        return null;
+        // get the directory name from stdout
+        String dir_name = new String(mdfpy.getInputStream().readAllBytes());
+        dir_name = dir_name.trim(); // Remove trailing newline
+
+        File dir = new File("./" + dir_name);
+
+        // get csv file names in directory
+        File[] files = dir.listFiles();
+        if (files == null) {
+            throw new Exception("Couldn't list in directory mdfpy should have made ('" + dir_name + "')");
+        }
+
+        // get absolute path of each file
+        List<String> paths = new ArrayList<String>();
+        for (File f: files) {
+            paths.add(f.getAbsolutePath());
+        }
+
+        return paths;
     }
 }
