0702cc6091f9e58617694006a0cd2907cc24f84a
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-03-05 22:01:41 +0000

    Checkout Serene's files from pythonjava branch

---
+++ pythonjava/exportcsv.py
@@ -0,0 +1,17 @@
+from sys import stdout
+from asammdf import MDF, Signal
+
+def main():
+    input_mdf = sys.argv[1]
+    input_dbc = sys.argv[2]
+    print(input_mdf)
+    print(input_dbc)
+    mdf = MDF(input_mdf)
+    databases = {"CAN": [(input_dbc, 1)]}
+    extracted = mdf.extract_bus_logging(database_files=databases)
+    speed = extracted.get("EngineSpeed")
+    extracted.export('csv', "out.csv")
+
+
+if __name__ == '__main__':
+    main()
