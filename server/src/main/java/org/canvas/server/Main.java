139acdd8b5a0ed40b0e10b7399550817347ce14f
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-03-06 01:12:46 +0000

    Move MdfImporter into server

--- server/src/main/java/org/canvas/server/Main.java
+++ server/src/main/java/org/canvas/server/Main.java
@@ -1,7 +1,14 @@
 package org.canvas.server;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Path mdf = Paths.get("../pythonjava/examples/audi/output1.mf4");
+        Path dbc = Paths.get("../pythonjava/examples/audi/audi.dbc");
+        File[] files = MdfImporter.convertMdfToCsvFiles(mdf, dbc);
+        System.out.println(MdfImporter.readCsvFileToSignalData(files[1]));
     }
 }
