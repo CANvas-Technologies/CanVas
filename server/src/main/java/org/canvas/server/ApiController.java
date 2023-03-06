1c3b148691d9635a6c2806c6846b643c253cff5b
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-03-06 21:15:11 +0000

    Add ApiController, gets trace UUID successfully

---
+++ server/src/main/java/org/canvas/server/ApiController.java
@@ -0,0 +1,35 @@
+package org.canvas.server;
+
+import java.sql.Connection;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ApiController {
+    @GetMapping("/api")
+    public String index() {
+        return "api";
+    }
+
+    @GetMapping(value = "/api/get_trace_uuid/{name}", produces = "text/plain")
+    @ResponseBody
+    public String getTraceUUID(@PathVariable("name") String name) {
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "candata", "postgres", "password");
+
+        String uuid = "";
+        try {
+            Connection connection = dcm.getConnection();
+            DatabaseDAO db = new DatabaseDAO(connection);
+
+            uuid = db.getTraceUUID(name);
+            System.out.println("GOT UUID: " + uuid);
+        } catch (Throwable e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        return uuid;
+    }
+}
