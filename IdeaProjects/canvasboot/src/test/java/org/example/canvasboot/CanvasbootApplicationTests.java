c1908ef16aa6eaf894383645ee858cb407ab429c
Supernova2002 <dstekol@gmail.com>
2023-03-06 14:49:04 -0500

    reworked retrieval stuff

---
+++ IdeaProjects/canvasboot/src/test/java/org/example/canvasboot/CanvasbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.canvasboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanvasbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
