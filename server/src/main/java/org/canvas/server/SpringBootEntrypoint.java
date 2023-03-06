d493633fa484ea62cd746ea075f622f12e29f584
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-03-06 20:43:05 +0000

    Run spotless:apply

--- server/src/main/java/org/canvas/server/SpringBootEntrypoint.java
+++ server/src/main/java/org/canvas/server/SpringBootEntrypoint.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootEntrypoint<TomcatConnectorCustomizer> {
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SpringBootEntrypoint.class, args);
-	}
-
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootEntrypoint.class, args);
+    }
 }
--- server/src/main/java/org/canvas/server/UploadHandler.java
+++ server/src/main/java/org/canvas/server/UploadHandler.java
@@ -48,5 +48,4 @@ public class UploadHandler {
 
         return trace;
     }
-
 }
