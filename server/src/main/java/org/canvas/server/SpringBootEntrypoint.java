2adc16fba474384b4d4ab11e3838f90dc3ec1b8a
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-03-06 20:06:00 +0000

    Add Spring Boot with basic upload
    Co-authored-by: serenemjoe <serene.joe@cooper.edu>

---
+++ server/src/main/java/org/canvas/server/SpringBootEntrypoint.java
@@ -0,0 +1,12 @@
+package org.canvas.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEntrypoint<TomcatConnectorCustomizer> {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootEntrypoint.class, args);
+	}
+
+}
