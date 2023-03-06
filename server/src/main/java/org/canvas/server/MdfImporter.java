2dfac11b920e75cfc45475001d0f7cd9a7fefbb9
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-03-06 03:37:59 +0000

    Minor cleanup

--- server/src/main/java/org/canvas/server/MdfImporter.java
+++ server/src/main/java/org/canvas/server/MdfImporter.java
@@ -1,11 +1,10 @@
 package org.canvas.server;
 
+import com.opencsv.CSVReader;
 import java.io.File;
 import java.io.FileReader;
 import java.nio.file.Path;
 import java.util.*;
-
-import com.opencsv.CSVReader;
 
 public class MdfImporter {
     // read in signal data, not bucketized
@@ -38,15 +37,19 @@ public class MdfImporter {
 
             if (!header[0].equals("timestamps")) {
                 csv.close();
-                throw new Exception("First column should be 'timestamps' but is actually " + header[0] + " " + path);
+                throw new Exception(
+                        "First column should be 'timestamps' but is actually "
+                                + header[0]
+                                + " "
+                                + path);
             }
 
             String signalName = header[1];
 
             System.out.println("Processing " + signalName);
 
-            // todo: for now, all signal values are doubles, but there can actually be different kinds
-            // List<Pair<Double, Double>> vals = new ArrayList<Pair<Double, Double>>();
+            // todo: for now, all signal values are doubles, but there can actually be different
+            // kinds
             List<Data> vals = new ArrayList<Data>();
 
             String[] line;
