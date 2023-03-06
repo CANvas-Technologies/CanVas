79970bbd775243a853d464a1274ed9bf61e9d40d
serenemjoe <serene.joe@cooper.edu>
2023-03-06 02:11:04 -0500

    Springboot part 2

---
+++ can/src/main/java/com/CANvas/can/CanApplication.java
@@ -0,0 +1,20 @@
+package com.CANvas.can;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+
+@SpringBootApplication
+public class CanApplication<TomcatConnectorCustomizer> {
+	private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(CanApplication.class, args);
+	}
+
+	}
+
+
+
