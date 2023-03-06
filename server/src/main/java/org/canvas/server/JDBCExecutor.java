51c26a2934fc5dd85e31b7348cee99fdb94d247c
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-03-06 19:31:29 +0000

    Run spotless:apply and don't reflow long strings

--- server/src/main/java/org/canvas/server/JDBCExecutor.java
+++ server/src/main/java/org/canvas/server/JDBCExecutor.java
@@ -24,9 +24,11 @@ public class JDBCExecutor {
             // according to this command:
 
             // trace_uuid length is 36 (uuid4 length with separator hyphens)
-            // trace_key_table name length is 47 chars - trace_UUID_keys, e.g. trace_9d01a158-88b2-46d4-bb97-e29f5a4d6c09_keys
+            // trace_key_table name length is 47 chars - trace_UUID_keys, e.g.
+            // trace_9d01a158-88b2-46d4-bb97-e29f5a4d6c09_keys
             //
-            // CREATE TABLE traces (trace_uuid char(36) PRIMARY KEY, trace_name varchar(1000), trace_key_table char(47));
+            // CREATE TABLE traces (trace_uuid char(36) PRIMARY KEY, trace_name varchar(1000),
+            // trace_key_table char(47));
 
             TraceHandle trace = newDAO.newTrace("actual_real_trace");
 
