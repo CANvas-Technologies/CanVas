ea5380763c675a44ef1e3c06c500ad0f9c0a178a
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-03-06 18:03:39 +0000

    Remove some varchar usage and store key table name in traces table

--- server/src/main/java/org/canvas/server/JDBCExecutor.java
+++ server/src/main/java/org/canvas/server/JDBCExecutor.java
@@ -22,8 +22,11 @@ public class JDBCExecutor {
             // SETUP INSTRUCTIONS:
             // Make sure your database is called candata, and you have a traces table premade
             // according to this command:
-            // CREATE TABLE traces (trace_number INT DEFAULT NULL PRIMARY KEY, trace_name
-            // varchar(50) DEFAULT NULL);
+
+            // trace_uuid length is 36 (uuid4 length with separator hyphens)
+            // trace_key_table name length is 47 chars - trace_UUID_keys, e.g. trace_9d01a158-88b2-46d4-bb97-e29f5a4d6c09_keys
+            //
+            // CREATE TABLE traces (trace_uuid char(36) PRIMARY KEY, trace_name varchar(1000), trace_key_table char(47));
 
             TraceHandle trace = newDAO.newTrace("actual_real_trace");
 
