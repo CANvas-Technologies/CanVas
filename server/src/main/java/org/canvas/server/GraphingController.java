7e21dd3f76d1683b5b3407e253260c8a495db801
Supernova2002 <dstekol@gmail.com>
2023-04-12 20:06:40 -0400

    literally just a starting graphing page

---
+++ server/src/main/java/org/canvas/server/GraphingController.java
@@ -0,0 +1,22 @@
+package org.canvas.server;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class GraphingController {
+    @GetMapping("/graphing")
+    public String index() {
+        return "graphing";
+    }
+}
+
