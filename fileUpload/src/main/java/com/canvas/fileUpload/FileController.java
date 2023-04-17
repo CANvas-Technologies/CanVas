c597a8e140b64080e69358c9d531aeff3fe9f2a9
serenemjoe <serene.joe@cooper.edu>
2023-04-17 07:42:23 -0400

    Add fileUpload

---
+++ fileUpload/src/main/java/com/canvas/fileUpload/FileController.java
@@ -0,0 +1,67 @@
+package com.canvas.fileUpload;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("files")
+public class FileController {
+
+    FileStorageProperties properties = new FileStorageProperties();
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @PostConstruct
+    public void init(){
+        fileStorageService =  new FileStorageService(properties);
+    }
+
+    @CrossOrigin
+    @PutMapping
+    public ResponseEntity<FileResponse> uploadFile(@RequestParam("file") MultipartFile file){
+        String fileName = fileStorageService.storeFile(file);
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/files/")
+                .path(fileName)
+                .toUriString();
+
+        FileResponse fileResponse = new FileResponse(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+        return new ResponseEntity<FileResponse>(fileResponse,HttpStatus.OK);
+    }
+
+    @GetMapping("/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName,HttpServletRequest request){
+
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        String contentType = null;
+
+        try {
+            contentType = request.getSession().getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        }catch(IOException ex) {
+            System.out.println("Could not determine fileType");
+        }
+
+        if(contentType==null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(resource);
+    }
+
+}
