faca324a8cfaf682b89acf14f125b3ffec359050
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-04-17 16:09:22 -0400

    Import fileUpload from fileUpload-v2

---
+++ fileUpload/src/main/java/com/canvas/fileUpload/FileStorageProperties.java
@@ -0,0 +1,19 @@
+package com.canvas.fileUpload;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix="file")
+public class FileStorageProperties {
+
+    private String uploadDir = "./uploads";
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
+
+
+}
