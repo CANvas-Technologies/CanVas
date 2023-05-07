97535dc57d833111fa2f89b2bd8b3431a8d76c87
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-05-07 17:21:57 -0400

    Add delete page

---
+++ frontend/src/components/pages/delete.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { ChangeEvent, useState} from 'react';
+import axios from 'axios';
+import cors from 'cors'
+
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+
+import TextField from '@mui/material/TextField';
+import Input from '@mui/material/Input';
+import { FormControl, FormLabel } from '@mui/material';
+
+function DeletePage() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [downloadUri, setDownloadUri] = React.useState();
+
+  async function doDelete() {
+    const uuid = await axios.get("http://localhost:8080/api/get_trace_uuid/" + name.value);
+    const response = await axios.get("http://localhost:8080/api/delete_trace/" + uuid.value);
+  }
+
+  return (
+    <center>
+    <br/>
+    <h1 className='upload'> Trace Deletion </h1>
+    <br/>
+    <FormControl>
+        {/* <FormLabel>Trace Deletion</FormLabel> */}
+        <TextField
+            value = {name}
+            onChange = {(event) => setName(event.target.value)}
+            label = 'Trace name'
+        />
+        <br/>
+        <TextField
+            value = {email}
+            onChange = {(event) => setEmail(event.target.value)}
+            label = 'User email'
+        />
+        <br/>
+        <Button onClick={doDelete} variant='contained' color='error'>Delete</Button>
+    </FormControl>
+    </center>
+  );
+}
+
+export default DeletePage;
