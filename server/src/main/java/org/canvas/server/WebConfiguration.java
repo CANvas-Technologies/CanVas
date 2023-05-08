3e7f3a8b43f23a2d148dfb93ee1d4d9f90da1977
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-05-08 16:21:35 -0400

    Attempt to webconfigure again

--- server/src/main/java/org/canvas/server/WebConfiguration.java
+++ server/src/main/java/org/canvas/server/WebConfiguration.java
@@ -2,8 +2,10 @@ package org.canvas.server;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
