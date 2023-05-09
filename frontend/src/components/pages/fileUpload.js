f46fb4e79826baebc6c7f2943024a0fab585ef08
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-05-09 13:30:09 -0400

    Add progress bar while uploading

--- frontend/src/components/pages/fileUpload.js
+++ frontend/src/components/pages/fileUpload.js
@@ -8,13 +8,15 @@ import Divider from "@mui/material/Divider";
 
 import TextField from "@mui/material/TextField";
 import Input from "@mui/material/Input";
-import { FormControl, FormLabel } from "@mui/material";
+import { FormControl, FormLabel, CircularProgress } from "@mui/material";
 
 function FileUploadSingle() {
   const [file, setFile] = useState();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [downloadUri, setDownloadUri] = React.useState();
+
+  const [uploading, setUploading] = React.useState(false);
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -24,38 +26,55 @@ function FileUploadSingle() {
 
   const uploadFile = async () => {
     try {
+      setUploading(true);
       const formData = new FormData();
       formData.append("file", file);
       const API_URL = "http://localhost:8080/upload/" + name + "/" + email;
       const response = await axios.post(API_URL, formData);
       setDownloadUri(response.data.fileDownloadUri);
+      setUploading(false);
     } catch (err) {
       alert(err.message);
     }
   };
 
   return (
-    <FormControl>
-      <FormLabel>Enter Name:</FormLabel>
-      <TextField
-        value={name}
-        onChange={(event) => setName(event.target.value)}
-        label="Trace name"
-      />
-      <br />
-      <TextField
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-        label="User email"
-      />
-      <br />
-      <Input type="file" onChange={handleFileChange} />
-      <br />
-      <Button onClick={uploadFile} size="large" variant="contained">
-        Upload
-      </Button>
-      <br />
-    </FormControl>
+    // if uploading, show progress bar
+    // else show upload button
+    <>
+      {uploading ? (
+        <>
+          <h2>Uploading...</h2>
+          <center>
+            <CircularProgress />
+          </center>
+        </>
+      ) : (
+        <>
+          <FormControl>
+            <FormLabel>Enter Name:</FormLabel>
+            <TextField
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              label="Trace name"
+            />
+            <br />
+            <TextField
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              label="User email"
+            />
+            <br />
+            <Input type="file" onChange={handleFileChange} />
+            <br />
+            <Button onClick={uploadFile} size="large" variant="contained">
+              Upload
+            </Button>
+            <br />
+          </FormControl>
+        </>
+      )}
+    </>
   );
 }
 
