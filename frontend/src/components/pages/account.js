0f0ddf56349f9ef89e2d9d3e1f717e37397f80aa
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-05-09 22:24:25 -0400

    List traces on account page

--- frontend/src/components/pages/account.js
+++ frontend/src/components/pages/account.js
@@ -1,12 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../../App.css";
 import Footer from "../footer";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  IconButton,
+  Avatar,
+} from "@mui/material";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SsidChartIcon from "@mui/icons-material/SsidChart";
 
+import { API_URL } from "../../env";
 import useUser from "../hooks/useUser";
+import authorizedGet from "../util/auth";
 
 export default function accountPage() {
   // get user state from useUser hook
   const { user, isLoading } = useUser();
+  const navigate = useNavigate();
+
+  const [traces, setTraces] = useState(null);
+
+  useEffect(() => {
+    if (!user) {
+      if (!isLoading) navigate("/login");
+      return;
+    }
+
+    const getTraces = async () => {
+      const response = await authorizedGet(
+        user,
+        `${API_URL}graphing/getTraceNames/${user.email}}`
+      );
+      setTraces(response.data);
+      console.log(response.data);
+    };
+
+    getTraces();
+  }, [user]);
+
+  const traceList = () => {
+    if (traces) {
+      return traces.map((trace) => {
+        console.log("making trace item...");
+        return (
+          <ListItem
+            secondaryAction={
+              <IconButton edge="end" aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
+            }
+          >
+            <ListItemAvatar>
+              <Avatar>
+                <SsidChartIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={trace} />
+          </ListItem>
+        );
+      });
+    }
+  };
 
   return (
     <>
@@ -19,6 +77,9 @@ export default function accountPage() {
           ? `Hello ${user.email}`
           : "You are not logged in"}
       </p>
+
+      <h1> Your Traces </h1>
+      <List>{traceList()}</List>
       <Footer />
     </>
   );
